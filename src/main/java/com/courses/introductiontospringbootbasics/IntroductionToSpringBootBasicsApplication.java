1e8e6b3f689374adb13259c3fb98266313f56b6c
Wael Antar <antarwael189@gmail.com>
2024-03-24 15:02:52 +0100

    Initial commit

---
+++ src/main/java/com/courses/introductiontospringbootbasics/IntroductionToSpringBootBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.courses.introductiontospringbootbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootBasicsApplication.class, args);
+	}
+
+}
